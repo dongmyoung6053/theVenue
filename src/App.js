da90b9291e6156c228499d1dc472f25f4f2b2985
dongmyoung kim <w0411191@nscc.ca>
2020-01-07 14:46:18 -0400

    The Venue

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './resources/styles.css';
+import { Element } from 'react-scroll';
+
+import Header from './components/header_footer/Header';
+import Featured from './components/featured';
+import VenueNfo from './components/venueNfo';
+import Highlights from './components/Highlights';
+import Pricing from './components/pricing';
+import Location from './components/location';
+import Footer from './components/header_footer/Footer';
+
+function App() {
+  return (
+    <div className="App" style={{ height: '1000px', background: 'cornflowerblue' }}>
+      <Header/>
+      <Element name="featured">
+        <Featured/>
+      </Element>
+      
+      <Element name="venuenfo">
+        <VenueNfo/>
+      </Element>
+      
+      <Element name="highlights">
+        <Highlights/>
+      </Element>
+
+      <Element name="pricing">
+        <Pricing/>
+      </Element>
+      
+      <Element name="location">
+        <Location/>
+      </Element>
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
